a198132c0718f5de02068644afcd09fee799b973
Ritika Sahay <113190073+SahayRitika@users.noreply.github.com>
2024-04-13 19:28:02 +0530

    Create numSquares.java

---
+++ numSquares.java
@@ -0,0 +1,13 @@
+class Solution {
+    public int numSquares(int n) {
+        int cntSq[]= new int[n+1];
+        Arrays.fill(cntSq,Integer.MAX_VALUE);
+        cntSq[0]=0;
+        for(int i=1;i<=n;i++){
+            for(int j=1;j*j<=i;j++){
+                cntSq[i] = Math.min(cntSq[i], cntSq[i-j*j]+1);
+            }
+        }
+        return cntSq[n];
+    }
+}
